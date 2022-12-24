09f0c456a3371e76d83d6965540d0b410adb830b
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2022-12-24 14:06:28 +0300

    Middleware

--- express.js/express-app/app.js
+++ express.js/express-app/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express(); //wil use the express functions w the app variable
 
-app.get('/', (req, res) => {
-    res.send('Hello User');
+app.use((req, res, next) => { //middleware function
+    console.log('First middleware');
+    next(); //allows the request to continue to the next middleware in line
 });
 
-app.get('/api/products', (req, res) => {
-    res.send(["product1", "product2", "product3"]);
+app.use((req, res, next) => { //middleware function
+    console.log('Second middleware');
+    res.send('<h1>Hello from Express!</h1>'); //sends a response to the browser
 });
 
 app.listen(3000, () => {

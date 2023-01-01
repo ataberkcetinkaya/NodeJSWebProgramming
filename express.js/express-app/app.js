8212b69cfd8737eac191d3bc851f19e67ba59029
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2023-01-01 20:48:54 +0300

    Routing

--- express.js/express-app/app.js
+++ express.js/express-app/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express(); //wil use the express functions w the app variable
 
-app.use((req, res, next) => { //middleware function
-    console.log('First middleware');
-    next(); //allows the request to continue to the next middleware in line
+app.use('/products', (req, res, next) => { //it's on top because it's specific location
+    res.send('<h1>Product Page</h1>');
 });
 
-app.use((req, res, next) => { //middleware function
-    console.log('Second middleware');
+app.use('/', (req, res, next) => { //middleware function => '/' with the path
     res.send('<h1>Hello from Express!</h1>'); //sends a response to the browser
 });
 

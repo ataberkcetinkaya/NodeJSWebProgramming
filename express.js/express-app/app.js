969bd22343a6fb9ddf107a8092a22868991bd3e6
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2023-01-04 23:22:30 +0300

    Body Parser

--- express.js/express-app/app.js
+++ express.js/express-app/app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express(); //wil use the express functions w the app variable
+const bodyParser = require('body-parser');
 
-app.use('/products', (req, res, next) => { //it's on top because it's specific location
-    res.send('<h1>Product Page</h1>');
+
+app.use(bodyParser.urlencoded({extended: false})); //will give us access to the body property on the request object
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+        <html>
+            <head><title>Add Product</title></head>
+            <body>
+                <form action="/product" method="POST">
+                    <input type="text" name="productName">
+                    <button type="submit">Add Product</button>
+            </body>
+        </html>
+        `);
+});
+
+app.post('/product', (req, res, next) => { //app.post will only listen to post requests
+    //database logic can be here
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => { //middleware function => '/' with the path

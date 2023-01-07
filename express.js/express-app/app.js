06b708b394fb175b926180764640a4c45e4c2918
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2023-01-07 19:36:49 +0300

    404 Error Page

--- express.js/express-app/app.js
+++ express.js/express-app/app.js
@@ -9,8 +9,12 @@ const userRoutes = require('./routes/user');
 app.use(bodyParser.urlencoded({extended: false}));
 
 //routes
-app.use('/admin', adminRoutes); //will use the adminRoutes
+app.use('/admin', adminRoutes); //will use the adminRoutes and /admin is the starting point => localhost:3000/admin/add-product
 app.use(userRoutes); //will use the userRoutes
+
+app.use((req, res, next) => { //if any of routes above are not used, this will be used
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
